1ac7905a3c7a65ae7345260a65632f25cdfd328d
Adityasinghbhadoriya <aditya0303singh@gmail.com>
2025-02-01 18:54:14 +0530

    mern-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TaskManager from './TaskManager';
+
+function App() {
+  return (
+   <div className='App'>
+       <h1><TaskManager/></h1>
+    </div>
+  );
+}
+
+export default App;
